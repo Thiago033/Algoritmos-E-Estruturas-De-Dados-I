688429631b7949c01a470105ab7f8fd81b3f47da
Thiago Lopes <88741214+Thiago033@users.noreply.github.com>
2022-08-08 16:26:06 -0300

    Updates exercicios

--- Exercicios-Revisao/Lista-9/exemplos/exercicio-026.c
+++ Exercicios-Revisao/Lista-9/exemplos/exercicio-026.c
@@ -1,3 +1,23 @@
+/*
+
+Implemente uma func¸ao que calcule as raızes de uma equacao do segundo grau do tipo
+Ax2 + Bx + C = 0. Lembrando que:
+
+A variavel A tem que ser diferente de zero.
+
+• Se ∆ < 0 nao existe real.
+• Se ∆ = 0 existe uma raiz real.
+• Se ∆ ≥ 0 existem duas ra´ızes reais.
+
+Essa funcao deve obedecer ao seguinte prototipo:
+int raizes(float A,float B,float C,float * X1,float * X2);
+
+Essa func¸ao deve ter como valor de retorno o numero de raızes reais e distintas da
+equacao. Se existirem raızes reais, seus valores devem ser armazenados nas variaveis
+apontadas por X1 e X2.
+
+*/
+
 #include <stdio.h>
 #include <math.h>
 
@@ -8,8 +28,11 @@ int main() {
 
     float a, b, c, raiz1, raiz2;
 
-    printf("A: ");
-    scanf("%f", &a);
+    do {
+        printf("A: (Nao pode ser zero)");
+        scanf("%f", &a);
+    } while (a == 0);
+    
     printf("B: ");
     scanf("%f", &b);
     printf("C: ");
