6997ebab331dd6aa57ac7882839960c6489c93b1
Thiago Lopes <88741214+Thiago033@users.noreply.github.com>
2022-08-12 22:46:53 -0300

    Exercicio 2

--- Lista-De-Exercicios-1/exercicio-2.c
+++ Lista-De-Exercicios-1/exercicio-2.c
@@ -1,52 +1,66 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define TAM 10
-#define MAX 100
+int main() {
+    char **nomes;
+    nomes = malloc(1 * sizeof(char *));
+    int opcao;
 
-char buffer[MAX];
+    char buffer[100];
+    int length = 0;
+    int i = 1;
+    int j = 0;
+    int indiceRemover;
 
-int main () {
-    char *nomes[TAM];
+    do {
+        printf("Digite uma opcao: \n");
+        scanf("%d", &opcao);
 
+        while (getchar() != '\n');
 
-    
+        if (opcao == 1) {
 
-    int i,j;
+            nomes = realloc(nomes, i * sizeof(char *));
+
+            printf("Digite um nome: \n");
+            fgets(buffer, 100, stdin);
+
+            length = strlen(buffer);
+            buffer[length-1] = '\0';
+
+            nomes[j] = malloc(length * sizeof(char));
+
+            strcpy(nomes[j], buffer);
+
+            i++;
+            j++;
+
+        } else if (opcao == 2) { 
+            for (int i = 0; i < j; i++) {
+                printf("nomes[%d] = %s \n", i , nomes[i]);
+            }
+
+        } else if (opcao == 3) { 
+            printf("Digite o indice do nome a ser removido: \n");
+            scanf("%d", &indiceRemover);
+            
+            for (int i = indiceRemover; i < j-1; ++i) {
+                nomes[i] = nomes[i+1];
+            }
+            
+            nomes = realloc(nomes, (j-1)*sizeof(char));
+
+            j--;
+
+        } else if (opcao == 0) {
+            printf("Saindo do programa!");
+            
+        } else {
+            printf("Opcao invalida!");
+
+        }
+    } while (opcao != 0);
+
+    return 0;
 }
-
-void adicionarNome(char *mat) {
-    int i = 0;
-
-    gets(buffer);
-    mat[i] = (char *) calloc((strlen(buffer)+1), sizeof(char));
-
-    if (!mat) {
-        printf ("** Erro: Memoria Insuficiente **");
-        exit(1);
-    }
-
-}
-
-    // printf("\nEntre com %d Frases:\n\n", TAM);
-    // for (i=0; i<TAM; i++) {
-
-    //     gets(buffer);
-    //     mat[i] = (char *) calloc((strlen(buffer)+1), sizeof(char));
-
-    //     if (!mat) {
-    //         printf ("** Erro: Memoria Insuficiente **");
-    //         for(j=0; j < i; j++)
-    //             free(mat[j]);
-    //         exit(1);
-    //     }
-
-    //     strcpy(mat[i],buffer);
-    // }   
-
-    // printf("\nFrases digitadas");
-    // for(i=0; i<TAM; i++)
-    // printf("%s\n", mat[i]);
-    // for(j=0; j<TAM; j++)
-    // free(mat[j]);
