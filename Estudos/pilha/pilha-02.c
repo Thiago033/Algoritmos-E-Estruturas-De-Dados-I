4755939c82c2956919263a6a480dd843b9b792a4
Thiago Lopes <88741214+Thiago033@users.noreply.github.com>
2022-09-08 09:01:04 -0300

    Create Pilha

---
+++ Estudos/pilha/pilha-02.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+struct Aluno
+{
+    int ra;
+    char nome[50];
+};
+
+struct Pilha
+{
+    Aluno *alunos;
+    Aluno *topo;
+    Aluno *base;
+    //int limite;
+};
+
+int main() {
+
+
+    return 0;
+}
---
+++ Exercicios/Exercicio-Pilha-1/exercicio-pilha-01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+typedef struct{
+    char nome[30];
+    int idade;
+};
+
+
+int main() {
+    
+    
+    return 0;
+}
