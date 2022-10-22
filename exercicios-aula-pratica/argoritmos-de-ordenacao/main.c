1a4211627d15b2dcc943845f63ecd86b7204d308
Thiago Lopes <88741214+Thiago033@users.noreply.github.com>
2022-10-22 14:59:37 -0300

    Merge Sort

--- exercicios-aula-pratica/argoritmos-de-ordenacao/main.c
+++ exercicios-aula-pratica/argoritmos-de-ordenacao/main.c
@@ -1,5 +1,66 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <math.h>
+
+/*
+===
+Merge Sort
+==
+*/
+int merge(int array[], int low, int mid, int high) {
+
+    int *temp, p1, p2, size, i, j, k;
+    int end1 = 0, end2 = 0;
+
+    size = high - low + 1;
+
+    p1 = low;
+    p2 = mid + 1;
+
+    temp = (int *) malloc(size * sizeof(int));
+
+    if (temp != NULL) {
+        for (i = 0; i < size; i++) {
+            if (!end1 && !end2) {
+                
+                if (array[p1] < array[p2]) {
+                    temp[i] = array[p1++];
+                } else {
+                    temp[i] = array[p2++];
+                }
+                
+                if (p1 > mid) end1 = 1;
+                if (p2 > high) end2 = 1;
+            } else {
+                if(!end1){
+                    temp[i] = array[p1++];
+                } else {
+                    temp[i] = array[p2++];
+                }
+            }
+        }
+
+        for (j = 0, k = low; j < size; j++, k++) {
+            array[k] = temp[j];
+        }
+    }
+    
+    free(temp);
+}
+
+void mergeSort(int *array, int low, int high) {
+
+    if (low < high) {
+
+        int mid = floor((low + high) / 2);
+
+        mergeSort(array, low , mid);
+        mergeSort(array, mid+1, high);
+        
+        merge(array, low, mid, high);
+    }
+}
 
 /*
 ===
